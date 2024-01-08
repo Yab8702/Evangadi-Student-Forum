48d5b2b3b6984b79ba2764bb6ad5e64398fddd24
yab8702 <yaleletdessalegn11@gmail.com>
2024-01-08 21:35:07 +0300

    update

--- server/server.js
+++ server/server.js
@@ -10,6 +10,9 @@ app.use("/api/answers", require("./routes/answerRoutes"));
 app.use("/api/images", require("./routes/imageRoutes"));
 app.use("/api/likes", require("./routes/likeRoutes"));
 app.use("/api/all/images", express.static(path.join(__dirname, "/images")));
+app.get("/", (req, res) => {
+  res.send("API is running....");
+});
 app.listen(3001, () => {
   console.log("Server is running on port 3001");
 });
